99f80bbeaed20b2c56e5893d3405f540fe531cab
Djouonang Landry <djouonanglandry@yahoo.com>
2017-05-27 19:42:26 +0100

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+
+//function to redirect to appropriate url
+
+function redirect($url) {
+
+    echo "<script language=\"JavaScript\">\n";
+    echo "<!-- hide from old browser\n\n";
+
+    echo "window.location = \"" . $url . "\";\n";
+
+    echo "-->\n";
+    echo "</script>\n";
+
+    return true;
+}
